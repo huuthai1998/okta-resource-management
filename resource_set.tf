a195291267ff2176532d85410f2f3f853e65fe48
huuthai1998 <thai_nguyen@mckinsey.com>
2024-01-30 20:53:29 +0700

    a

--- resource_set.tf
+++
@@ -1,5 +0,0 @@
-resource "okta_resource_set" "resource_set" {
-  label       = local.resource_set_label
-  description = "Resource set for ${var.label}"
-  resources   = [format("%s/api/v1/apps/%s", local.org_url, var.oidc_app_id), ]
-}
